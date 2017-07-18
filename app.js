d0cee75b219cd2ca6d69c4ece74dddec321a24c6
“rogrenke” <roland@rolandgrenke.com>
2017-07-18 11:22:23 +0100

    Initial mongodb setup

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ var users = require('./routes/users');
 
 var app = express();
 
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/to-learn-list_development';
+mongoose.connect(mongoDB);
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
