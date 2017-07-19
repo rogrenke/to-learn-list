ad1d4ebf2b0c1849290383611805df01a031f9e1
Toby Winter <toby.hw@live.co.uk>
2017-07-19 11:40:45 +0100

    Created User model and unit test in userSpec
    Adds
    - validator
    - md5
    - mongoose-mongodb-errors
    - passport-local-mongoose
    
    uses above tools to write user model with validation and useful error messages

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: 'Please provide an email address',
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid email address']
+  },
+  name: {
+    type: String,
+    required: 'Please provide a name',
+    trim: true
+  }
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email'});
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
--- test/userSpec.js
+++ test/userSpec.js
@@ -1,11 +1,32 @@
-var app = require('../app')
+var app = require('../app');
 var mongoose = require('mongoose');
+var chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect;
 
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+
+describe('User', function() {
+  var User = require('../models/user');
+
+  describe('User creation', function() {
+    it('creates user', function(done) {
+      var testUser = new User({
+        name: 'Testy',
+        email: 'testy@test.com',
+        password: 'testytest'
+      });
+      testUser.save(function() {
+        User.find({ email: 'testy@test.com' }, function(err, users) {
+          expect(users[0].name).to.equal('Testy');
+          done();
+        });
+      });
+    });
+
+    after(function(done) {
+      mongoose.connection.collections.users.drop(function() {
+        done();
+      });
     });
   });
 });
