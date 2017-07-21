90b09829ad3bdf0c3c69ba24dd72906a7540e98b
Mário Ribeiro <mail@marioribeiro.com>
2017-07-21 16:44:24 +0100

    Add method to updateItem status

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -7,5 +7,18 @@ exports.createItem = async (req, res, next) => {
   const newItem = new Item(req.body);
   await newItem.save();
   res.redirect(`/lists/${req.params.id}`);
-  next()
+  next();
 };
+
+exports.updateItem = async (req, res, next) => {
+  console.log(req.query.status);
+  let newStatus;
+  if (req.query.status == 'incomplete') {
+    newStatus = 'complete'
+  } else {
+    newStatus = 'incomplete'
+  }
+  const updatedItem = await Item.findOneAndUpdate({ _id: req.query.item }, { status: newStatus }, { new: true });
+  res.redirect(`/lists/${updatedItem.list}`);
+  next();
+}
