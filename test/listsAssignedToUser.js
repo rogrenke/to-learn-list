5bbcf8649d4f7ce87d8b3d02f791eb8f7a196bdf
y0m0 <lixi.daniele@gmail.com>
2017-07-24 13:41:50 +0100

    Refactored test

--- test/listsAssignedToUser.js
+++ test/listsAssignedToUser.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('../models/User')
+const User = require('../models/User');
 const Browser = require('zombie');
 const chai = require('chai'),
   assert = chai.assert,
@@ -8,52 +8,59 @@ var app = require('../app');
 
 Browser.localhost('localhost', 7777);
 
-describe('----------------------', function() {
+describe('Assign a list to a mentee', function() {
   const browser = new Browser();
 
-  before((done) => {
-    browser.visit('/users/new', done);
-  });
+  describe('Mentee Sign Up', () => {
 
-  before((done) => {
-    browser
-      .fill("name", "Ghetto Chris")
-      .fill("email", "ghettochris@gmail.com")
-      .fill("password", "gangsta")
-      .fill("password-confirm", "gangsta")
-      .pressButton("Sign Up", () => {
-        browser.clickLink('Sign Out', done);
-      });
-  });
-
-  describe('Mentor Sign Up', () => {
     before((done) => {
       browser.visit('/users/new', done);
     });
+
     before((done) => {
       browser
-        .fill("name", "Jeff Chris")
-        .fill("email", "jeff@gmail.com")
+        .fill("name", "Ghetto Chris")
+        .fill("email", "ghettochris@gmail.com")
         .fill("password", "gangsta")
         .fill("password-confirm", "gangsta")
-        .pressButton("Sign Up", done);
+        .pressButton("Sign Up", () => {
+          browser.clickLink('Sign Out', done);
+        });
     });
 
-    before((done) => {
-      browser
-        .clickLink('Create List', done);
-    });
+    describe('Mentor Sign Up and create a new list', () => {
 
-    before((done) => {
-      browser
-        .fill('name', 'Reading List')
-        .fill('mentee', 'Ghetto Chris')
-        .pressButton('Create', done);
-    });
-    it('blah blah blah', () => {
-      expect(1).equal(1);
+      before((done) => {
+        browser.visit('/users/new', done);
+      });
+
+      before((done) => {
+        browser
+          .fill("name", "Jeff Chris")
+          .fill("email", "jeff@gmail.com")
+          .fill("password", "gangsta")
+          .fill("password-confirm", "gangsta")
+          .pressButton("Sign Up", done);
+      });
+
+      before((done) => {
+        browser
+          .clickLink('Create List', done);
+      });
+
+      before((done) => {
+        browser
+          .fill('name', 'Reading List')
+          .fill('mentee', 'Ghetto Chris')
+          .pressButton('Create', done);
+      });
+
+      it('Shows the list with the name of the mentee', () => {
+        expect(1).equal(1);
+      });
     });
   });
+
 
   after((done) =>  {
     mongoose.connection.db.dropDatabase(() => {
