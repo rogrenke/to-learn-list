c264cd84681d76ac72635097c938e5daaac3631c
Christopher Harrop <bannastre@gmail.com>
2017-07-25 20:36:21 +0100

    UserSpec refactor complete

--- test/userSpec.js
+++ test/userSpec.js
@@ -4,29 +4,40 @@ const chai = require('chai'),
   assert = chai.assert,
   expect = chai.expect;
 
+
 describe('User', () => {
   const User = mongoose.model('User');
 
+  afterEach((done) => {
+    dbCleaner.clean(mongoose.connection.db, () => {
+      done()
+    })
+  })
+
   it('Creates a new user', async () => {
-    const user = await new User({
-      name: 'test_user',
+    var user = await new User({
+      name: 'testUser',
       email: 'user@test.com',
       password: 'testPassword'
     }).save()
-    const users = User.find({}, (err, users) => {
+    var users = User.find({}, (err, users) => {
       expect(users[0].name).to.equal(user.name)
     })
   })
 
-  // it('throws an error if an email is not provided', async () => {
-  //   const user = await new User({
-  //     name: 'test_user',
-  //     password: 'testPassword'
-  //   }).save()
-  //   const users = User.find({}, (err, users) => {
-  //     expect(users[0].name).to.equal(user)
-  //   })
-  // })
+  it('throws an error if an email is not provided', () => {
+    expect(new User({
+      name: 'testUser',
+      password: 'testPassword'
+    })).to.throw
+  })
+
+  it('throws an error if a name is not provided', () => {
+    expect(new User({
+      email: 'user@test.com',
+      password: 'testPassword'
+    })).to.throw
+  })
 })
 
 describe('Testing', () => {
@@ -34,50 +45,3 @@ describe('Testing', () => {
     assert.true
   })
 })
-
-afterEach((done) => {
-  dbCleaner.clean(mongoose.connection.db, () => {
-    done()
-  })
-})
-
-//
-//   describe('User validation', () => {
-//
-//     it('throws an error if an email is not provided', (done) => {
-//       var testUser = new User({ name: 'testing', password: 'test_password' });
-//       testUser.validate((err) => {
-//         expect(err.errors.email).to.exist;
-//         expect(err.errors.email.properties.message).to.equal('Please provide an email address');
-//         done();
-//       });
-//     });
-//
-//     it('throws an error if a name is not provided', (done) => {
-//       var testUser = new User({ email: 'testy@test.com', password: 'test_password' });
-//       testUser.validate((err) => {
-//         expect(err.errors.name).to.exist;
-//         expect(err.errors.name.properties.message).to.equal('Please provide a name');
-//         done();
-//       });
-//     });
-//
-//     it('does not create a user if email is not unique', (done) => {
-//       var newUser = new User({
-//         name: 'newuser',
-//         email: 'test@test.com',
-//         password: 'somethingelse'
-//       });
-//       newUser.validate((err) => {
-//         expect(err.errors).to.exist;
-//         done();
-//       });
-//     });
-//
-//     after((done) =>  {
-//       mongoose.connection.db.dropDatabase(() => {
-//         done();
-//       });
-//     });
-//   });
-// });
