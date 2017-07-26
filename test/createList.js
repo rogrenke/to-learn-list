9e1449ffd8ae017c6e431f4ac3cbb0dbbbdef1e4
Toby Winter <toby.hw@live.co.uk>
2017-07-26 15:36:50 +0100

    Creates a list - test refactored (1st)

--- test/createList.js
+++ test/createList.js
@@ -1,101 +1,108 @@
-// const app = require('../app');
-// const Browser = require('zombie');
-// const chai = require('chai'),
-//   assert = chai.assert,
-//   expect = chai.expect;
-// const helpers = require('./helpers');
-// const mongoose = require('mongoose');
+const app = require('../app');
+const Browser = require('zombie');
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect;
+const helpers = require('./helpers');
+const mongoose = require('mongoose');
+
+const browser = new Browser();
+const User = mongoose.model('User');
+const List = mongoose.model('List');
+
+Browser.localhost('localhost', 7777);
+
+describe('List', () => {
+
+  it( 'creates a list', async() => {
+    const mentor = await new User({
+      name: 'Mentor',
+      email: 'mentor@test.com'
+    }).save()
+    const mentee = await new User({
+      name: 'Mentee',
+      email: 'mentee@test.com'
+    }).save()
+    const list = await new List({
+      name: 'New List',
+      mentor: mentor._id,
+      mentee: mentee._id
+    }).save()
+    var lists = List.find({}, (err, lists) => {
+      if (err) {
+        console.log('Error in finding lists', err);
+      } else {
+        expect(lists[0].name).to.equal('New List')
+      }
+    })
+  })
+
+  // it('creates a list', () => {
+  //   var lists = List.find({}, (err, lists) => {
+  //     if (err) {
+  //       console.log('Error in finding lists', err);
+  //     } else {
+  //       expect(lists[0].name).to.equal('New List')
+  //     }
+  //   })
+
+})
+
+describe('Testing', () => {
+  it('passed', () => {
+    assert.true
+  })
+})
+
 //
-// const browser = new Browser();
-// const User = mongoose.model('User');
-// const List = mongoose.model('List');
+//   describe('User visits the create list page from the homepage', () => {
 //
-// Browser.localhost('localhost', 7777);
+//     before((done) => {
+//       browser.clickLink('Create List', done);
+//     });
 //
-// describe('List', () => {
+//     it('should get the new list page', () => {
+//       browser.assert.text('h1', 'Create List');
+//       browser.assert.url({ pathname: '/lists/new' });
+//     });
 //
-//   before( async() => {
-//     const mentor = await new User({
-//       name: 'listMentor',
-//       email: 'listMentor@test.com',
-//       password: 'listTestPassword'
-//     })
+//     it('should display the form', () =>{
+//       browser.assert.element('form');
+//     });
+//   });
+// });
 //
-//     const mentee = await new User({
-//       name: 'listMentee',
-//       email: 'listMentee@test.com',
-//       password: 'listTestPassword'
-//     })
+// describe('List Creation form', () => {
+//   const browser = new Browser();
 //
-//     const list = await new List({
-//       name: 'New List',
-//       mentor: mentor.name,
-//       mentee: mentee.name
-//     })
-//   })
+//   before((done) => {
+//     helpers.createUserAndSignOut("Ghetto Chris", "chris@mail.com", "gangsta", browser, done);
+//   });
 //
-//   it('creates a list', () => {
-//     var lists = List.find({}, (err, lists) => {
-//       expect(lists[0].name).to.equal(list.name)
-//     })
-//   })
-// })
+//   before((done) => {
+//     helpers.createUser("Jeff Jones", "jeff@mail.com", "password", browser, done);
+//   });
 //
-// describe('Testing', () => {
-//   it('passed', () => {
-//     assert.true
-//   })
-// })
+//   after((done) =>  {
+//     mongoose.connection.db.dropDatabase(() => {
+//       done();
+//     });
+//   });
 //
-// //
-// //   describe('User visits the create list page from the homepage', () => {
-// //
-// //     before((done) => {
-// //       browser.clickLink('Create List', done);
-// //     });
-// //
-// //     it('should get the new list page', () => {
-// //       browser.assert.text('h1', 'Create List');
-// //       browser.assert.url({ pathname: '/lists/new' });
-// //     });
-// //
-// //     it('should display the form', () =>{
-// //       browser.assert.element('form');
-// //     });
-// //   });
-// // });
-// //
-// // describe('List Creation form', () => {
-// //   const browser = new Browser();
-// //
-// //   before((done) => {
-// //     helpers.createUserAndSignOut("Ghetto Chris", "chris@mail.com", "gangsta", browser, done);
-// //   });
-// //
-// //   before((done) => {
-// //     helpers.createUser("Jeff Jones", "jeff@mail.com", "password", browser, done);
-// //   });
-// //
-// //   after((done) =>  {
-// //     mongoose.connection.db.dropDatabase(() => {
-// //       done();
-// //     });
-// //   });
-// //
-// //   describe('User creates a new list', () => {
-// //     before((done) => {
-// //       browser.clickLink('Create List', done);
-// //     });
-// //
-// //     before((done) => {
-// //       browser
-// //         .fill('name', 'Reading List')
-// //         .fill('mentee', 'Ghetto Chris')
-// //         .pressButton('Create', done);
-// //     });
-// //
-// //     it('should display the new list', () => {
-// //       browser.assert.text('h1', 'Reading List');
-// //     });
-// //   });
-// // });
+//   describe('User creates a new list', () => {
+//     before((done) => {
+//       browser.clickLink('Create List', done);
+//     });
+//
+//     before((done) => {
+//       browser
+//         .fill('name', 'Reading List')
+//         .fill('mentee', 'Ghetto Chris')
+//         .pressButton('Create', done);
+//     });
+//
+//     it('should display the new list', () => {
+//       browser.assert.text('h1', 'Reading List');
+//     });
+//   });
+// });
